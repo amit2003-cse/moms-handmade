f079747a102a1444e467f0421135e5a968c93bf0
Amit Kumar <amit4321sg@gmail.com>
2026-03-19 12:37:12 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import Hero from '../components/home/Hero';
 import Features from '../components/home/Features';
 import Categories from '../components/home/Categories';
 import Newsletter from '../components/home/Newsletter';
 
 const Home = () => {
+
+  useEffect(() => {
+    // API call karne ke liye fetch ka use
+    fetch('https://moms-handmade.onrender.com')
+      .then(() => {
+        console.log('backend started');
+      })
+      .catch((error) => {
+        console.error('Error starting backend:', error);
+      });
+  }, []); // Khali array ka matlab hai ye sirf pehli baar chalega
+
   return (
     <div className="overflow-x-hidden">
       <Hero />
